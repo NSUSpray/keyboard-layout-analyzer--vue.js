8f66a9dd08b76c81387dbadb0631bafacecb5cca
NSUSpray <nsu.spray@gmail.com>
2024-08-17 23:23:07 +0600

    Combine DropButton parts into a single component

--- src/components/DropButton.vue
+++ src/components/DropButton.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import DropButtonMenu from './_DropButtonMenu.vue'
-import ToggleButton from './_ToggleButton.vue'
+import { onMounted, ref } from 'vue'
+import { transitionDurationOf } from '../lib/browser.js'
 
 defineExpose({ focus: () => main.value.focus() })
 
@@ -16,6 +15,25 @@ const shortkey = props.v_shortkey
     && JSON.parse(props.v_shortkey.replaceAll('\'', '"'))
 
 const main = ref(null)
+const menu = ref(null)
+const isFadeOut = ref(false)
+
+onMounted(() => fadeOutOnClick('a, button, .btn, input'))
+
+function fadeOut(event, fadeOutTime) {
+  isFadeOut.value = true
+  setTimeout(() => event.target.blur(), 1)
+  setTimeout(() => isFadeOut.value = false, fadeOutTime)
+}
+
+function fadeOutOnClick(selector) {
+  const items = menu.value.querySelectorAll(selector)
+  const fadeOutLag = 35  // hand-picked value
+  const fadeOutTime = transitionDurationOf(menu.value) + fadeOutLag
+  items?.forEach(item =>
+      item.addEventListener('click', event => fadeOut(event, fadeOutTime))
+  )
+}
 </script>
 
 <template>
@@ -24,16 +42,26 @@ const main = ref(null)
     (no v-else, no <template>). Otherwise, any click event will bubble up.
   -->
   <div v-if="!onClick" class="button-group">
-    <ToggleButton ref="main" :disabled :title>{{ value }}
-      <DropButtonMenu><slot /></DropButtonMenu>
-    </ToggleButton>
+    <button type="button" ref="main" class="toggle" :disabled :title
+        @click="e => e.target.focus()">
+      {{ value }}
+      <div ref="menu" class="menu" :class="{ 'fade-out': isFadeOut }">
+        <slot />
+      </div>
+      <span class="caret" />
+    </button>
   </div>
-  <div v-if="onClick" class="button-group"><!-- separate toggle -->
+  <!-- separate toggle -->
+  <div v-if="onClick" class="button-group">
     <button type="button" ref="main" :disabled :title
         @click="onClick" v-shortkey="shortkey">{{ value }}</button>
-    <ToggleButton :disabled>
-      <DropButtonMenu><slot /></DropButtonMenu>
-    </ToggleButton>
+    <button type="button" class="toggle" :disabled
+        @click="e => e.target.focus()">
+      <div ref="menu" class="menu" :class="{ 'fade-out': isFadeOut }">
+        <slot />
+      </div>
+      <span class="caret" />
+    </button>
   </div>
 </template>
 
@@ -41,5 +69,61 @@ const main = ref(null)
 .button-group {
   display: inline-block;
   position: relative;
+}
+
+.toggle { cursor: default; }
+.toggle:not(:first-child) {
+  padding: calc(var(--narrow-padding) * 0.8);
+}
+
+.caret {
+  display: inline-block;
+  margin: 0;
+  border: solid 4px transparent;
+  border-top-color: #FFF;
+}
+.toggle:first-child .caret {
+  margin-left: var(--margin);
+  margin-right: calc(var(--thin-margin) * -2);
+}
+
+.menu {
+  position: absolute;
+  left: 0;
+  top: calc(100% + var(--thin-padding) * 0.6);
+  width: max-content;
+  padding: var(--thin-padding) 0;
+  text-align: left;
+  background-color: var(--black-blue);
+  border-radius: var(--radius);
+  box-shadow: var(--shadow);
+  z-index: 2;
+  transition-duration: var(--transition-duration);
+  visibility: hidden;
+  opacity: 0;
+  & > * {
+    display: block;
+    padding:
+        calc(var(--thin-padding) * 0.6)
+        var(--wide-padding);
+    color: white;
+  }
+  & > :hover,
+  & > :focus {
+    text-decoration: none;
+    color: white;
+    background-color: var(--dark-blue);
+  }
+}
+
+.button-group:last-child .menu {
+  left: auto;
+  right: 0;
+}
+
+:is(:hover, :focus):not([disabled]) > .menu:not(.fade-out),
+    .menu:is(:hover, :has(:focus)):not(.fade-out) {
+  visibility: initial;
+  opacity: 1;
 }
 </style>
