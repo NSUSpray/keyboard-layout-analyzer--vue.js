feb2d5f5f2f22c08a4cced5a53b3c3e2b75cacf8
NSUSpray <nsu.spray@gmail.com>
2024-12-03 18:51:31 +0600

    KeyDialog: highlight fields with invalid values

--- src/components/KeyDialog.vue
+++ src/components/KeyDialog.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import Fingers from '../lib/fingers.js'
 import { objectFlip } from '../lib/utilities.js'
 
@@ -13,8 +13,17 @@ const dialog = ref(null)
 // current state
 
 const cValues = {
-  primary: ref('a'), shift: ref('A'),
-  altGr: ref('á'), shiftAltGr: ref('Á'),
+  primary: { label: 'Primary' },
+  shift: { label: 'Shift' },
+  altGr: { label: 'Alt Gr' },
+  shiftAltGr: { label: 'Shift + Alt Gr' },
+}
+for (const cValue of Object.values(cValues)) {
+  cValue.ref = ref('')
+  cValue.code = computed
+      (() => keySetCode(cValue.ref.value))
+  cValue.isValid = computed
+      (() => cValue.ref.value === '' || cValue.code.value !== undefined)
 }
 const cFinger = ref(Fingers.LEFT_INDEX)
 const isStartOfFinger = ref(false)  // start position of finger
@@ -23,11 +32,10 @@ const wasStartOfFinger = ref(false)
 let lastStartOfFinger, cId
 
 for (const [oKey, cValue] of Object.entries(cValues))
-    watch(cValue, newValue => {
+    watch(cValue.ref, () => {
       if (cId === undefined) return  // ignore on show
-      const code = keySetCode(newValue)
-      if (code === undefined) return
-      keySet.value.keys[cId][oKey] = code
+      if (!cValue.isValid.value) return
+      keySet.value.keys[cId][oKey] = cValue.code.value
     })
 
 watch(cFinger, newValue => {
@@ -48,7 +56,7 @@ const codeFor = { esc: 27, backspace: 8, tab: 9, del: 127, capslock: 20,
 const reprFor = objectFlip(codeFor)
 
 function inputRepr(code) {
-  if (code === undefined) return
+  if (code === undefined) return ''
   return reprFor[code] ?? String.fromCharCode(code)
 }
 
@@ -63,7 +71,7 @@ function show(index) {
   if (dialog.value.open) close()
   const key = keySet.value.keys[index]
   for (const oKey in cValues)
-      cValues[oKey].value = inputRepr(key[oKey])
+      cValues[oKey].ref.value = inputRepr(key[oKey])
   cFinger.value = key.finger
   lastStartOfFinger = keySet.value.fingerStart[key.finger]
   isStartOfFinger.value = wasStartOfFinger.value =
@@ -88,17 +96,10 @@ const capsToTitle = caps =>
 <template>
   <dialog ref="dialog" @cancel="close">
     <div>
-      <label>Primary:
-        <input type="text" v-model="cValues.primary.value" />
-      </label>
-      <label>Shift:
-        <input type="text" v-model="cValues.shift.value" />
-      </label>
-      <label>Alt Gr:
-        <input type="text" v-model="cValues.altGr.value" />
-      </label>
-      <label>Shift + Alt Gr:
-        <input type="text" v-model="cValues.shiftAltGr.value" />
+      <label v-for="[key, cValue] of Object.entries(cValues)" :key>
+        {{ cValue.label }}:
+        <input type="text" v-model="cValue.ref.value"
+            :class="{ danger: !cValue.isValid.value }" />
       </label>
     </div>
     <label>Finger for Pressing Key:
